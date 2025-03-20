b023fee3a6c21c75c3b1534524eef341503da7e3
venkat mamidi <mamidivenkatprasad@gmail.com>
2025-03-20 11:54:08 -0400

    DTO and MessageFunction classes added

---
+++ message/src/main/java/com/example/message/functions/MessageFunction.java
@@ -0,0 +1,22 @@
+package com.example.message.functions;
+
+import com.example.message.dto.FunctionDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Slf4j
+@Configuration
+public class MessageFunction {
+
+    @Bean
+    public Function<FunctionDto,FunctionDto> email() {
+        return accountsMsgDto -> {
+            log.info("Sending email with the details : " +  accountsMsgDto.toString());
+            return accountsMsgDto;
+        };
+    }
+
+}
